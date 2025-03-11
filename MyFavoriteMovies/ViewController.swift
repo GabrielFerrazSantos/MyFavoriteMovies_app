f73caa8ac09b3d7fd4cacb1851f0becd70bd54fd
Gabriel Ferraz dos Santos <gabriel.ferrazsantos@hotmail.com>
2025-03-11 10:10:47 -0300

    Initial Commit

---
+++ MyFavoriteMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavoriteMovies
+//
+//  Created by Gabriel Ferraz dos Santos on 11/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
